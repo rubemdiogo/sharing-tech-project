a7e4d9c449a7e8347c51a2ed655765b525f77c03
Rubem Diogo <rubemdiogo@gmail.com>
2020-09-21 12:26:58 -0400

    rotas, signup

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,14 +1,20 @@
-const mongoose = require('mongoose');
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/sharing-tech-project';
+const mongoose = require("mongoose");
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/sharing-tech-project";
 
 mongoose
   .connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
   })
-  .then(() => console.log(`Successfully connected to the database ${MONGODB_URI}`))
-  .catch(error => {
-    console.error(`An error ocurred trying to connect to the database ${MONGODB_URI}: `, error);
+  .then(() =>
+    console.log(`Successfully connected to the database ${MONGODB_URI}`)
+  )
+  .catch((error) => {
+    console.error(
+      `An error ocurred trying to connect to the database ${MONGODB_URI}: `,
+      error
+    );
     process.exit(1);
   });
