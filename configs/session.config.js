e1b8949bc676aa2c58a1626ea0a1b465bdd13922
Rubem Diogo <rubemdiogo@gmail.com>
2020-09-21 17:48:37 -0400

    CRUD, rotas, forms

--- configs/session.config.js
+++ configs/session.config.js
@@ -10,7 +10,7 @@ module.exports = (app) => {
       secret: process.env.SESS_SECRET,
       resave: false,
       saveUninitialized: true,
-      cookie: { maxAge: 60000 },
+      cookie: { maxAge: 600000 },
       store: new MongoStore({
         mongooseConnection: mongoose.connection,
         ttl: 60 * 60 * 24,
